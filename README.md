0ea3360d309c70f100c840564ea32b36efbe798c
ojer <cacaojer@outlook.com>
2025-10-28 13:50:18 +0800

    fix: 功能完善

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # vite-plugin-vue-dync-page
 
-每次打包生成不同的首页文件
+每次打包生成带版本号的 index.html
+
+生成编译版本号 `build-version.json`
+
+复制原 `index.html` 为 `index_.html`
+
+将原 `index.html` 重命名为 `index_编译版本号.html`
+
+将插件预设内容写入新的 `index.html`
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { readFileSync, renameSync, writeFileSync, copyFileSync } from "node:fs"
-import { join } from "node:path"
+import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const pluginIndexPath = join(__dirname, "index.html")
+const pluginIndexHtml = readFileSync(pluginIndexPath, "utf8")
 
 async function plugin() {
   const dyncPage = {
     name: "vite-plugin-vue-dync-page",
     writeBundle({ dir }, bundle) {
-      const fileName = "index.html"
-      const inFile = bundle[fileName]
+      const indexHtml = "index.html"
+      const inFile = bundle[indexHtml]
       if (inFile && inFile.fileName) {
         const buildVersion = new Date(Date.now() + 1000 * 60 * 60 * 8)
           .toISOString()
@@ -16,14 +21,12 @@ async function plugin() {
           join(dir, "build-version.json"),
           JSON.stringify({ build: buildVersion }),
         )
-        const index = join(dir, fileName)
-        const refrush = join(dir, `index_${buildVersion}.html`)
-        const back = join(dir, `_${fileName}`)
-        renameSync(index, refrush)
-        // renameSync(back, index)
-        const htmlStr = readFileSync("./index.html", "utf8")
-        writeFileSync(index, htmlStr)
-        copyFileSync(refrush, back)
+        const indexFile = join(dir, indexHtml)
+        const idnexVersionFile = join(dir, `index_${buildVersion}.html`)
+        const indexHtmlBackup = join(dir, `_${indexHtml}`)
+        copyFileSync(indexFile, indexHtmlBackup)
+        renameSync(indexFile, idnexVersionFile)
+        writeFileSync(indexFile, pluginIndexHtml)
       }
     },
   }
